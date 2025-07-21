2ec8cb3af2ec7e61d8ea77ef2901931462e0cf69
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-07-21 12:07:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-const url = 'https://api.themoviedb.org/3/authentication/token/new';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNWE2ZTI1MTE3ZjJjNWNjMTE5NTM5OWUzZWExM2VlZCIsIm5iZiI6MTc1MjY1NTk5Mi44MzEsInN1YiI6IjY4Nzc2ODc4YWZjMDgyNTY5NTE2NWNiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3dBsUWtpB8dMCHuGheSe1ZCRX1E4SQYkjT1gf6aUBA'
+// Your API Key from TMDb
+const API_KEY = "d5a6e25117f2c5cc1195399e3ea13eed";
+
+// Base URL for TMDb API
+const BASE_URL = "https://api.themoviedb.org/3";
+
+// Popular movies endpoint
+const endpoint = `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+// Function to fetch data
+const fetchMovies = async () => {
+  try {
+    const response = await fetch(BASE_URL + endpoint);
+    const data = await response.json();
+
+    console.log("Data", data);
+
+    // Check if the response is successful
+    if (response.ok) {
+      displayMovieData(data);
+    } else {
+      console.log("Error fetching data:", data.status_message);
+    }
+  } catch (error) {
+    console.log("Error:", error);
   }
 };
+fetchMovies();
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log("Movie: ",json))
-  .catch(err => console.error(err));
+function displayMovieData(data) {
+  let movieSection = document.getElementById("movie-section");
+  data.results.map((item) => {
+    let movieCard = document.createElement("div");
+    movieCard.className = "movie-card";
+    movieCard.innerHTML = `
+              <a href="https://www.themoviedb.org/movie/${item.id}" class="a-movie">
+                <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="${item.title}">
+                <div class="movie-card-info">
+                    <p class="title">${item.title}</p>
+                </div>
+              </a>
+    `;
+    movieSection.appendChild(movieCard)
+  });
+}
